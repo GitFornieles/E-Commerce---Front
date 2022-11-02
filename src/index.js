d56ac6debffd89af7714af05bbefaac3413310cc
Martin Hofmann <martinhofmann@Martins-MacBook-Pro.local>
2022-11-02 11:58:17 -0300

    server levantado correctamente

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../App";
-import "./index.css";
+import App from "../src/App";
+// import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
