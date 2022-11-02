c6e4b1f3ba8dab1004d04b5e875272fe937e7c8a
gerar10 <gerarburgos1987@gmail.com>
2022-11-02 14:43:51 -0300

    configuracion inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div className="App">
+    holaaaa
+    </div>
+  );
+}
 
 export default App;
