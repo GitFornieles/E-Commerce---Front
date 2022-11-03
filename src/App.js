a01bb22f7c5e36af102e37a9d3f52b6658ddcc30
gerar10 <gerarburgos1987@gmail.com>
2022-11-03 18:13:11 -0300

    gridproductos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import GrillaDeProductos from './components/GrillaDeProductos';
 
 function App() {
   return (
     <div className="App">
-    holaaaa
+      
     </div>
   );
 }
