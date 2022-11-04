439ff4a858a7bea6cdf4cf7f09b0226f42568986
Martin Hofmann <martinhofmann@Martins-MacBook-Pro.local>
2022-11-04 14:39:43 -0300

    Componente Home creado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Logo from "../img/logo.jpg";
 import "../Styles/Navbar.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -7,12 +6,6 @@ import { useSelector } from "react-redux";
 const Navbar = () => {
   const user = useSelector((state) => state.user);
 
-  const imgStyles = {
-    heigth: "150px",
-    width: "150px",
-    objectFit: "cover",
-    objectPosition: " center",
-  };
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -23,9 +16,11 @@ const Navbar = () => {
                 Registro
               </button>
             </Link>
-            <button type="button" className="btn btn-secondary btn-sm">
-              Login
-            </button>
+            <Link to="/login">
+              <button type="button" className="btn btn-secondary btn-sm">
+                Login
+              </button>
+            </Link>
           </div>
           <div className="form-inline my-2 my-lg-0">
             <input
@@ -40,23 +35,6 @@ const Navbar = () => {
           </button>
         </div>
       </nav>
-      <div className="corner-ribbon">
-        <div className="corner-ribbon__inner">
-          <div className="corner-ribbon__ribbon"></div>
-        </div>
-        <div id="text" className="m-0 row justify-content-center">
-          <h5>SAHUMEROS ARTESANALES </h5>
-          <p> Sahumerios suaves y diferentes. Hechos artesanalmente</p>
-        </div>
-        <div className="m-0 row justify-content-center">
-          <img src={Logo} style={imgStyles} alt="logo" />
-        </div>
-      </div>
-      <div className="separator">
-        <div className="separator__content"> </div>
-
-        <div className="separator__separator"></div>
-      </div>
     </div>
   );
 };
