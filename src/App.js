fca5040fd9065bfcaceeed9fd9a88389fcdc4cf1
Martin Hofmann <martinhofmann@Martins-MacBook-Pro.local>
2022-11-09 23:14:25 -0300

    creacion del componente para agregar productos al catalogo

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from "./components/Home";
 import Login from "./components/User/LogIn";
 import AdminLogIn from "./components/Admin/AdminLogIn";
 import UsersList from "./components/Admin/UsersList";
+import AddProducts from "./components/Admin/AddProducts";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login/admin" element={<AdminLogIn />} />
         <Route path="/admin/all_users" element={<UsersList />} />
+        <Route path="/admin/add_product" element={<AddProducts />} />
       </Routes>
     </div>
   );
---
+++ src/components/Admin/AddProducts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddProducts = () => {
+  return <div></div>;
+};
+
+export default AddProducts;
