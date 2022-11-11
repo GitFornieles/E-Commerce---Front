b445b534add89ef0ba7b59f9f63da35f306a15e1
Martin Hofmann <martinhofmann@Martins-MacBook-Pro.local>
2022-11-11 13:07:43 -0300

    refactorizando

--- src/components/Admin/AdminProducts.jsx
+++ src/components/Admin/AdminProducts.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import AdminProductCard from "../../commons/AdminProductCard";
+import { Link } from "react-router-dom";
 
 const AdminProducts = () => {
   return (
     <div>
-      <div>
-        <nav aria-label="breadcrumb">
-          <ol className="breadcrumb">
-            <li className="breadcrumb-item">
-              <a href="http://localhost:3000/">Home</a>
-            </li>
-          </ol>
-        </nav>
-      </div>
       <div id="text" className="m-0 row justify-content-center ">
         <div className="bg-danger">
           <h5>Administración de Producto </h5>
         </div>
         <h6> Editar/eliminar producto</h6>
+      </div>
+      <div>
+        <Link to="/">
+          <button className="btn btn-secondary btn-sm btn-color">
+            {" "}
+            ↩ Home{" "}
+          </button>
+        </Link>
       </div>
       <AdminProductCard />
     </div>
